5142bcd664b28e365852758d0dae02582f449a81
Aditya Oza <89646381+AD2509@users.noreply.github.com>
2022-10-20 20:33:32 +0530

    Initial commit

---
+++ src/components/Add.js
@@ -0,0 +1,44 @@
+import React, {useState} from "react";
+import {Button,Form} from "react-bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css";
+import Employess from "./Employees";
+import {v4 as uuid} from "uuid";
+import {Link,useNavigate} from 'react-router-dom'
+
+function Add(){
+
+    const[name,setName] = useState('');
+    const[age,setAge] = useState('');
+
+    let history = useNavigate();
+
+    const handleSubmit=(e) => {
+        e.preventDefault();
+
+        const ids = uuid();
+        let uniqueId = ids.slice(0,8);
+
+        let a = name,
+        b = age;
+
+        Employess.push({id:uniqueId,Name : a, Age : b});
+
+        history("/");
+         
+    }
+    return <div>
+        <Form className="d-grid gap-2" style={{margin:"15rem"}}>
+            <Form.Group className="mb-3" controlId="formName">
+                <Form.Control type="text" placeholder="Enter Name" required onChange={(e) => setName(e.target.value)}>
+                </Form.Control>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formAge">
+                <Form.Control type="text" placeholder="Enter Age" required onChange={(e) => setAge(e.target.value)}>
+                </Form.Control>
+            </Form.Group>
+            <Button onClick={(e) => handleSubmit(e)} type="submit">Submit</Button>
+        </Form>
+    </div>;
+    
+}
+export default Add;
---
+++ src/components/Employees.js
@@ -0,0 +1,18 @@
+const Employees = [
+    {
+        id:"1",
+        Name:"Aditya Oza",
+        Age:"22",
+    },{
+
+    id:"2",
+    Name:"Omkar sharma",
+    Age:"23",
+},{ 
+ 
+    id:"3",
+    Name:"Lucky gawle",
+    Age:"24",
+}
+]
+export default Employees;
